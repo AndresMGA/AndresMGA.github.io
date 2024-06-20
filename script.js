71faff336eef09ea4251b49a5b82fc4366b61a4e
AndresMGA <andresmga@hotmail.com>
2024-06-20 15:06:27 +0100

    clean unnecesary

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
 let players = []
 
 class MidiPlayer {
-  constructor(mainWrapper, container, midiFile) {
-    // set up properties
-    this.mainWrapper = document.querySelector(mainWrapper);
-    this.container = document.querySelector(container);
+  constructor(  midiFile) {
     this.midiFile = midiFile;
     this.player;
-
     this.seq;
-
-    // create a main container
-    this.mainContainer = this.createMainContainer();
-
-    // move the passed container into the main container
-    this.mainContainer.appendChild(this.container);
-
-
-    // create the buttons wrapper
-    this.buttons = this.createButtonWrapper();
-
-    // create the buttons and tempo slider
-    this.playBtn = this.createButton("play");
-    this.pauseBtn = this.createButton("pause");
-    this.stopBtn = this.createButton("stop");
-
-    this.rewindBtn = this.createButton("rewind");
-    this.forwardBtn = this.createButton("forward");
-
-
+    this.mainContainer = document.getElementById("buttons-container")
+    this.buttons = document.getElementById("buttons")
+    this.playBtn = document.getElementById("play");
+    this.pauseBtn =document.getElementById("pause");
+    this.stopBtn = document.getElementById("stop");
+    this.rewindBtn =document.getElementById("rewind");
+    this.forwardBtn = document.getElementById("forward");
     this.loadFile();
   }
 
@@ -66,18 +49,15 @@ class MidiPlayer {
 
   loadSequence() {
     let that = this;
-    
     this.player = new mm.SoundFontPlayer(
       "https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus"
     );
     this.player.callbackObject = {
       run(note) {
-
         that.callback(note);
       },
       stop() {}
     };
-
     this.setListeners();
   }
 
@@ -126,136 +106,47 @@ class MidiPlayer {
         this.playBtn.classList.remove("hide");
         this.pauseBtn.classList.add("hide");
         this.pauseBtn.classList.remove("active");
-        this.container.scrollLeft = 0;
       }
     });
-
-
-  }
-
-  // constructor UI functions ////////////////////////////////////////
-
-
-  createMainContainer() {
-    let mc = document.createElement("div");
-    mc.className = "container d-flex flex-column bd-highlight my-4";
-    let mainContainer = this.container.parentNode;
-    mainContainer.insertBefore(mc, this.container);
-    return mc;
-  }
-
-  createButtonWrapper() {
-    let wr = document.createElement("div");
-    wr.className = "buttons hide";
-    this.mainContainer.appendChild(wr);
-    return wr;
-  }
-
-  createButton(txt) {
-    let btn = document.createElement("i");
-
-    switch (txt) {
-      case "pause":
-        btn.className = `fa-solid fa-circle-pause active hide ${txt}`;
-        break;
-      case "play":
-        btn.className = `fa-solid fa-circle-play ${txt}`;
-        break;
-      case "rewind":
-        btn.className = `fa-solid fa-backward ${txt}`;
-        break;
-      case "forward":
-          btn.className = `fa-solid fa-forward ${txt}`;
-          break;
-      case "stop":
-            btn.className = `fa-solid fa-refresh ${txt}`;
-            break;
-
-
-    }
-
-    this.buttons.appendChild(btn);
-    return btn;
-  }
-
-
-
-
-  createFileSelector() {
-    // file selector wrapper
-    let fs = document.createElement("div");
-    fs.className = "file-select-wrapper input-group mb-3";
-    // inner wrapper
-    let cs = document.createElement("div");
-    cs.className = "custom-file";
-    // input field that needs to be returned
-    let gf = document.createElement("input");
-    gf.setAttribute("type", "file");
-    gf.setAttribute("accept", ".mid");
-    // label
-    let lb = document.createElement("label");
-    lb.className = "custom-file-label";
-    lb.setAttribute("aria-describedby", "getFileAddOn");
-    lb.textContent = "Choose midi file";
-    cs.appendChild(lb);
-    cs.appendChild(gf);
-    fs.appendChild(cs);
-    this.mainContainer.appendChild(fs);
-
-    return gf;
   }
 }
 
-async function getInitialMidiFile(){
-  const currentUrl = window.location.href;
-
-// Create a URL object
-const url = new URL(currentUrl);
-
-// Get the query string part of the URL
-const queryString = url.search; // This includes the '?' character
-
-// Remove the '?' character if you just want the parameters
-const queryParams = queryString.substring(1);
-
-console.log(queryParams);
+async function fetchMidiFile(queryParams){
   let response = await fetch('https://AndresMGA.github.io/scores/'+queryParams+'/file.mid');
-  const svgContainer = document.getElementById('mySvg');
-
-  fetch('https://AndresMGA.github.io/scores/'+queryParams+'/file.svg')
-    .then(response => response.text())
-    .then(svgText => {
-      svgContainer.innerHTML = svgText;
-    })
-    .catch(error => console.error('Error fetching SVG: ', error));
   let data = await response.blob();
-  let container = document.createElement("div");
-
-  let id = _.random(1, 1000000000);
-  container.setAttribute("id", `mid-${_.random(1, 1000000000)}`);
-  document.getElementById("main-container").appendChild(container);
-  let obj = new MidiPlayer(
-    "#main-container",
-    `#${container.id}`,
-    data
-  );
+  let obj = new MidiPlayer( data );
   players.push(obj);
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  const fileInput = document.getElementById('get-file');
-  if (fileInput) {
-    fileInput.addEventListener('change', (e) => {
-      if( e.target.files.length === 0 ) return;
-      let container = document.createElement('div')
-      let id = _.random(1, 1000000000)
-      container.setAttribute('id',`mid-${_.random(1, 1000000000)}`)
-      document.getElementById('main-container').appendChild(container)
-      let lastIndex = e.target.files.length-1
-      let obj = new MidiPlayer('#main-container',`#${container.id}`,e.target.files[lastIndex])
-      players.push(obj);
-    });
+async function fetchSVG(queryParams) {
+ 
+    // Fetch the SVG file
+    const response = await fetch('https://AndresMGA.github.io/scores/'+queryParams+'/file.svg');
+    const svgText = await response.text();
+
+    // Create a new SVG element
+    const svgContainer = document.getElementById("svg-container");
+
+
+    // Append the SVG text as a child of the container
+    svgContainer.innerHTML = svgText;
+
+    // Adjust SVG size to fit its container
+    const svgElement = svgContainer.querySelector('svg');
+    if (svgElement) {
+      const containerWidth = svgContainer.clientWidth;
+      const containerHeight = svgContainer.clientHeight;
+      console.log(window.innerHeight)
+      svgElement.setAttribute('width', "100%");
+      svgElement.setAttribute('height', "800px");
+    }
   }
-  getInitialMidiFile()
-});
+
+  const currentUrl = window.location.href;
+  const url = new URL(currentUrl);
+  const queryString = url.search; 
+  const queryParams = queryString.substring(1);
+  fetchMidiFile(queryParams);
+  fetchSVG(queryParams);
+
 
